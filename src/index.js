e9c79ee00af49ddcc0ee67d4def4875ba6c3986c
dende-h <84645413+dende-h@users.noreply.github.com>
2021-10-30 08:04:21 +0900

    deleteエリア追加、todo追加処理実装

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import "./styles.css";
+
+//ボタンをクリックしたときの処理を関数定義
+const clickButton = () => {
+  //id="input-todo"に入力した内容をinputTextとして変数に取得
+  const inputText = document.getElementById("input-todo").value;
+
+  //id="input-todo"に入力した内容を消去
+  document.getElementById("input-todo").value = "";
+
+  //id="todo-list"の中に入力したTODOを追加
+  const todo = addTodo(inputText);
+  document.getElementById("todo-list").appendChild(todo);
+};
+
+//id="add-button"のクリック時にclickButton関数を動かす
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => clickButton());
+
+//<div>タグの生成<p>の生成　=>　入力した内容を<p>タグに入れ<div>タグの子要素にして返却
+const addTodo = (text) => {
+  const div = document.createElement("div");
+  div.className = "list-row";
+  const p = document.createElement("p");
+  p.innerText = text;
+  div.appendChild(p);
+  return div;
+};
