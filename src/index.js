c1748840cd9a7470cb501c64d1b8f99efd37c05b
dende-h <84645413+dende-h@users.noreply.github.com>
2021-11-04 11:39:07 +0900

    コード内メモ追加

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import "./styles.css";
 
+//id="add-button"のクリック時にclickButton関数を動かす
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => clickButton());
+
 //ボタンをクリックしたときの処理を関数定義
 const clickButton = () => {
   //id="input-todo"に入力した内容をinputTextとして変数に取得
@@ -12,17 +17,12 @@ const clickButton = () => {
     //id="todo-list"の中に入力したTODOを追加
     const todo = addTodo(inputText);
     document.getElementById("todo-list").appendChild(todo);
+    //ダブルクリックで編集できるイベントを付与
     todo.children[0].addEventListener("dblclick", (event) => onDblClick(event));
-    seveData(todo.children[0]);
   } else {
     alert("TODOを入力してください");
   }
 };
-
-//id="add-button"のクリック時にclickButton関数を動かす
-document
-  .getElementById("add-button")
-  .addEventListener("click", () => clickButton());
 
 //<div>タグの生成<p>の生成　=>　入力した内容を<p>タグに入れ<div>タグの子要素にして返却
 const addTodo = (text) => {
@@ -30,7 +30,6 @@ const addTodo = (text) => {
   div.className = "list-row";
   div.id = "drag-target";
   div.draggable = "true";
-  div.ondragstart = "event.dataTransfer.setData('text/plain',null)";
   const p = document.createElement("p");
   p.id = "todoText";
   p.innerText = text;
@@ -38,25 +37,27 @@ const addTodo = (text) => {
   return div;
 };
 
+//ダブルクリックイベントの関数定義
 const onDblClick = (event) => {
-  const editTarget = event.target;
-  console.log(editTarget);
-  const div = editTarget.parentNode;
-  console.log(div);
-  const todoText = div.children[0].innerText;
-  const edit = document.createElement("textarea");
-  edit.value = todoText;
-  edit.id = "edit-zone";
-  div.parentNode.appendChild(edit);
+  //<p>タグ要素を消去して<textarea>タグ要素を追加
+  const div = event.target.parentNode;
+  const todoText = event.target.innerText;
+  const editForm = document.createElement("textarea");
+  editForm.value = todoText;
+  editForm.id = "edit-zone";
+  div.parentNode.appendChild(editForm);
   div.parentNode.removeChild(div);
+  //編集して要素外をクリックすると編集が確定
   document
     .getElementById("edit-zone")
     .addEventListener("change", (event) => onEdit(event));
+  //チェンジイベントの関数定義
   const onEdit = (event) => {
     const editText = event.target;
     const newDiv = addTodo(editText.value);
     editText.parentNode.appendChild(newDiv);
     editText.parentNode.removeChild(editText);
+    //ダブルクリックイベント付与
     newDiv.children[0].addEventListener("dblclick", (event) =>
       onDblClick(event)
     );
@@ -64,46 +65,36 @@ const onDblClick = (event) => {
 };
 
 //ドラッグ＆ドロップ
-/* events fired on the draggable target */
+//ドラッグターゲット用変数
 let dragged;
 
-/* events fired on the draggable target */
-document.addEventListener("drag", (event) => {}, false);
+//ドロップエリアの定義オブジェクト（分岐用）
+const dropErea = {
+  todoE: "todo-area dropzone",
+  doingE: "doing-area dropzone",
+  doneE: "done-area dropzone",
+  delE: "title dropzone-d"
+};
+const { todoE, doingE, doneE, delE } = dropErea;
 
+//ドラッグスタートしたときに発火
 document.addEventListener(
   "dragstart",
   (event) => {
-    // store a ref. on the dragged elem
+    // 移動する要素を変数に設定
     dragged = event.target;
-    // make it half transparent
+    //ドラッグされる要素を半透明にする
     event.target.style.opacity = 0.5;
   },
   false
 );
 
-document.addEventListener(
-  "dragend",
-  (event) => {
-    // reset the transparency
-    event.target.style.opacity = "";
-  },
-  false
-);
-
-/* events fired on the drop targets */
-document.addEventListener(
-  "dragover",
-  (event) => {
-    // prevent default to allow drop
-    event.preventDefault();
-  },
-  false
-);
-
+//ドラッグがドロップ可能なエリアに入ったときに発火
+//ドロップエリアの背景カラーを変更
 document.addEventListener(
   "dragenter",
   (event) => {
-    // highlight potential drop target when the draggable element enters it
+    //各エリアのルート分岐
     switch (event.target.className) {
       case todoE:
         event.target.style.background = "rgb(54, 255, 255)";
@@ -124,11 +115,31 @@ document.addEventListener(
   },
   false
 );
+//ドラッグ操作が終了した際に発火
+document.addEventListener(
+  "dragend",
+  (event) => {
+    //ドラッグした要素の半透明を初期化
+    event.target.style.opacity = "";
+  },
+  false
+);
 
+//有効なドロップ先の上でドラッグしている状態で発火
+document.addEventListener(
+  "dragover",
+  (event) => {
+    //ブラウザのデフォルトの設定を無効化
+    event.preventDefault();
+  },
+  false
+);
+
+//ドラッグ要素が有効なドロップ先から出た時に発火
 document.addEventListener(
   "dragleave",
   (event) => {
-    // reset background of potential drop target when the draggable element leaves it
+    //背景カラーの初期化
     switch (event.target.className) {
       case todoE:
         event.target.style.background = "";
@@ -150,21 +161,25 @@ document.addEventListener(
   false
 );
 
+//有効なドロップ先にドロップされた時に発火
 document.addEventListener(
   "drop",
   (event) => {
-    // prevent default action (open as link for some elements)
+    //デフォルトの設定を無効化
     event.preventDefault();
-    // move dragged elem to the selected drop target
+    //ドロップ先へ要素を移動させる処理
     let dropZone;
     const onDrop = () => {
+      //背景カラーの初期化
       event.target.style.background = "";
+      //ドラッグした要素をもとの場所から削除
       dragged.parentNode.removeChild(dragged);
+      //ドロップで追加する場所を定義
       dropZone = event.target.children[1];
-      console.log(dropZone);
-      console.log(dragged);
+      //ドロップ先に追加
       dropZone.appendChild(dragged);
     };
+    //ドロップ先のルート分岐処理
     switch (event.target.className) {
       case todoE:
         onDrop();
@@ -176,6 +191,7 @@ document.addEventListener(
         onDrop();
         break;
       case delE:
+        //deleteエリアにドロップした際は要素を削除
         event.target.style.background = "";
         event.target.parentNode.style.background = "";
         dragged.parentNode.removeChild(dragged);
@@ -186,14 +202,8 @@ document.addEventListener(
   },
   false
 );
-const dropErea = {
-  todoE: "todo-area dropzone",
-  doingE: "doing-area dropzone",
-  doneE: "done-area dropzone",
-  delE: "title dropzone-d"
-};
-const { todoE, doingE, doneE, delE } = dropErea;
 
+//セーブ機能の実装途中
 const seveData = () => {
   const lists = document.querySelectorAll("p");
   let todos = [];
